cb7bf6e9167e99980e6a4dccf52bba2f0dc2630f
ny-julian-g <julian.gasser@noseryoung.ch>
2026-05-20 16:18:19 +0200

    add remaining methods

--- src/app/VehicleRentalManager.java
+++ src/app/VehicleRentalManager.java
@@ -1,27 +1,37 @@
 package app;
 
-import exceptions.ObjectAlreadyInListException;
+import exceptions.*;
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
 
 public class VehicleRentalManager {
     private List<Vehicle> vehicles;
+    private List<Vehicle> availableVehicles;
     private List<Contract> contracts;
     private List<Person> customerList;
     private List<Person> denyList;
 
-    public void addPersonToDenyList(Person person) {
-        if (person == null ) throw new NullPointerException("No person defined in parameter");
-        else if (isInDenyList(person)) throw new ObjectAlreadyInListException("Person is already in deny list");
-        else denyList.add(person);
+    public VehicleRentalManager(List<Vehicle> vehicles, List<Vehicle> availableVehicles, List<Contract> contracts,
+                                List<Person> customerList, List<Person> denyList){
+        this.vehicles = vehicles;
+        this.availableVehicles = availableVehicles;
+        this.contracts = contracts;
+        this.customerList = customerList;
+        this.denyList = denyList;
     }
 
-    public boolean isInDenyList(Person person) {
-        if (person == null ) throw new NullPointerException("No person defined in parameter");
-        for (Person findPerson : denyList) {
-            if (findPerson.equals(person)) return true;
-        }
-        return false;
+    public VehicleRentalManager() {
+        this.vehicles = new ArrayList<>();
+        this.availableVehicles = new ArrayList<>();
+        this.contracts = new ArrayList<>();
+        this.customerList = new ArrayList<>();
+        this.denyList = new ArrayList<>();
+    }
+
+    public void addPersonToDenyList(Person person) {
+        if (objectIsExistingInList(person, denyList)) throw new ObjectAlreadyInListException("Person is already in deny list");
+        else denyList.add(person);
     }
 
     public Contract createContract(Person customer,
@@ -29,20 +39,44 @@ public class VehicleRentalManager {
                                LocalDate startDate,
                                LocalDate endDate,
                                String condition) {
-        if (customer == null)throw new NullPointerException("No person defined in parameter");
-        if (vehicle == null)throw new NullPointerException("No vehicle defined in parameter");
+        if (customer == null) throw new NullPointerException("No person defined in parameter");
+        if (vehicle == null) throw new NullPointerException("No vehicle defined in parameter");
         if (startDate == null) throw new NullPointerException("No start-date defined in parameter");
-        if (endDate == null ) throw new NullPointerException("No end-date defined in parameter");
+        if (endDate == null) throw new NullPointerException("No end-date defined in parameter");
         if (condition == null) condition = "No condition defined";
-        return contracts.add(new Contract(customer, vehicle, startDate, endDate, condition));
+        Contract contract = new Contract(customer, vehicle, startDate, endDate, condition);
+        contracts.add(contract);
+        availableVehicles.remove(vehicle);
+        return contract;
     }
 
     public void addVehicle(Vehicle vehicle){
         if (vehicle == null) throw new NullPointerException ("No vehicle defined in parameter");
-        for (Vehicle findVehicle: vehicles){
-            if (findVehicle.licensePlate.equals(vehicle.licensePlate)) throw new
+        if (objectIsExistingInList(vehicle, vehicles)) throw new
                     ObjectAlreadyInListException("License plate already registered");
-        }
         vehicles.add(vehicle);
+        availableVehicles.add(vehicle);
+    }
+    public void returnVehicle(Vehicle vehicle){
+        //not implemented yet
+    }
+
+    public void addCustomer(Person customer){
+        if (objectIsExistingInList(customer, customerList))
+            throw new ObjectAlreadyInListException("Customer already registered");
+       customerList.add(customer);
+    }
+
+    public List<Vehicle> getAvailableVehicles(){
+        if (availableVehicles == null) throw new NullPointerException("No Vehicles available");
+        return availableVehicles;
+    }
+
+    public boolean objectIsExistingInList(Object o, List<?> list){
+        if (o == null ) throw new NullPointerException("The passed parameter object is null");
+        for (Object findObject: list) {
+            if (findObject.equals(o)) return true;
+        }
+        return false;
     }
 }
