3c2d04524d00d3cc2f116626ff86fb66cfb7b396
ny-noe-e <noe.eberli@noseryoung.ch>
2026-05-20 14:35:57 +0200

    implemented Person creation

--- src/app/App.java
+++ src/app/App.java
@@ -1,7 +1,23 @@
 package app;
 
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
 public class App {
-    public void test(){
-        System.out.println("test");
+
+    public void PersonRun(){
+        Personcreate();
+
+    }
+
+    public void Personcreate(){
+        List<Person> persons = new ArrayList<>();
+        persons.add(new Person(LocalDate.of(1995, 4, 12), "Muster", "Max", "Bahnhofstrasse 1"));
+        persons.add(new Person(LocalDate.of(2001, 6, 4), "Mueller", "Marcel", "Geerenstrasse 14"));
+
+        for (Person person : persons) {
+            System.out.println(person);
+        }
     }
 }
--- src/app/Person.java
+++ src/app/Person.java
@@ -7,4 +7,17 @@ public class Person {
     String name;
     String firstName;
     String adress;
+
+    public Person(LocalDate birthYear, String name, String firstName, String adress) {
+        this.birthYear = birthYear;
+        this.name = name;
+        this.firstName = firstName;
+        this.adress = adress;
+    }
+
+    @Override
+    public String toString() {
+        return firstName + " " + name + ", " + birthYear + ", " + adress;
+    }
+
 }
