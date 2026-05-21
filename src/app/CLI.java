0eb00caeb44507e9195dabbe80203bbecaf726bb
ny-julian-g <julian.gasser@noseryoung.ch>
2026-05-21 16:07:07 +0200

    add different detailed menus

--- src/app/CLI.java
+++ src/app/CLI.java
@@ -23,24 +23,75 @@ public class CLI {
         System.out.println("                          ~-_/_/                  ~~ ~~");
         System.out.println("WELCOME TO OUR GARAGE");
     }
-
-    protected static int menu(){
-        System.out.println("-  [1] Customer");
-        System.out.println("-  [2] Vehicles");
-        System.out.println("-  [3] Renting");
-        System.out.println("-  [0] Exit");
+    protected static int validateInput(int min, int max){
         int input = 0;
         try{
             input = parseInt(sc.nextLine());
         }catch (Exception e){
             System.out.println("Please enter again");
-            menu();
+            return -1;
         }
         if (input >= 0 && input < 4)return input;
         else{
             System.out.println("Please enter again");
-            menu();
+            return -1;
         }
-        return -1;
     }
+
+    protected static void menu(){
+        System.out.println("-  [1] Customer");
+        System.out.println("-  [2] Vehicles");
+        System.out.println("-  [3] Renting");
+        System.out.println("-  [0] Exit");
+        int input = -1;
+        do {input = validateInput(0,3);
+        } while (input != -1);
+        switch (input){
+            case 0: return;
+            case 1: customerMenu(); break;
+            case 2: vehicleMenu(); break;
+            case 3: rentingMenu(); break;
+        }
+    }
+
+    protected static int customerMenu(){
+        System.out.println("#----Customer Menu----#");
+        System.out.println("- [1] Add Customer");
+        System.out.println("- [2] Remove Customer");
+        System.out.println("- [3] Edit Customer");
+        System.out.println("- [4] Display Customer");
+        System.out.println("- [5] Ban Customer");
+        System.out.println("- [0] Back");
+        int input = -1;
+        do {input = validateInput(0,5);
+        } while (input != -1);
+        return input;
+    }
+
+
+    protected static int vehicleMenu(){
+        System.out.println("#----Vehicle Menu----#");
+        System.out.println("- [1] Add Vehicle");
+        System.out.println("- [2] Remove Vehicle");
+        System.out.println("- [3] Edit Vehicle");
+        System.out.println("- [4] Display Vehicle");
+        System.out.println("- [0] Back");
+        int input = -1;
+        do {input = validateInput(0,5);
+        } while (input != -1);
+        return input;
+    }
+
+    protected static int rentingMenu(){
+        System.out.println("#----Renting Menu----#");
+        System.out.println("- [1] Display Contracts");
+        System.out.println("- [2] Make Contract");
+        System.out.println("- [3] End Contract");
+        System.out.println("- [0] Back");
+        int input = -1;
+        do {input = validateInput(0,3);
+        } while (input != -1);
+        return input;
+    }
+
 }
